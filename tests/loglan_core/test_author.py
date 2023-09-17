40f0471952b0f6c8af38d82705b70d73f74326e1
torrua <torrua@users.noreply.github.com>
2023-09-17 15:41:13 +0700

    update tests

--- tests/loglan_core/test_author.py
+++ tests/loglan_core/test_author.py
@@ -1,10 +1,10 @@
 import pytest
 from loglan_core import Author
-
+from sqlalchemy import select
 
 @pytest.mark.usefixtures("db_session")
 class TestAuthor:
-
     def test_contribution(self, db_session):
-        author = db_session.query(Author).filter(Author.id == 1).first()
+        stmt = select(Author).where(Author.id == 1)
+        author = db_session.execute(stmt).scalar()
         assert len(author.contribution) == 4
