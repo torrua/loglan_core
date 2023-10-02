c3daf82e766ff5db9c1b55b935fde78fb3007228
torrua <torrua@users.noreply.github.com>
2023-10-02 22:31:50 +0700

    Add Exporter class with tests

---
+++ loglan_core/addons/exporter.py
@@ -0,0 +1,195 @@
+# -*- coding: utf-8 -*-
+"""
+This module contains an "Export extensions" for LOD dictionary SQL model.
+Add export() function to db object for returning its text string presentation.
+"""
+
+from loglan_core.definition import BaseDefinition
+from loglan_core.event import BaseEvent
+from loglan_core.setting import BaseSetting
+from loglan_core.syllable import BaseSyllable
+from loglan_core.type import BaseType
+from loglan_core.word import BaseWord
+from loglan_core.author import BaseAuthor
+from loglan_core.word_spell import BaseWordSpell
+
+
+class Exporter:
+    """General Exporter Class"""
+    @classmethod
+    def export(cls, obj):
+        """general export function"""
+
+        exporters = {
+            BaseAuthor: cls.export_author,
+            BaseDefinition: cls.export_definition,
+            BaseEvent: cls.export_event,
+            BaseSetting: cls.export_setting,
+            BaseSyllable: cls.export_syllable,
+            BaseType: cls.export_type,
+            BaseWord: cls.export_word,
+            BaseWordSpell: cls.export_word_spell,
+        }
+
+        exporter_func = exporters.get(obj.__class__)
+        if exporter_func:
+            return exporter_func(obj)
+
+        raise ValueError(f"Unsupported object type: {obj.__class__}")
+
+
+    @staticmethod
+    def export_author(obj: BaseAuthor) -> str:
+        """
+        Prepare Author data for exporting to text file
+        Returns:
+            Formatted basic string
+        """
+        return f"{obj.abbreviation}@{obj.full_name}@{obj.notes}"
+
+    @staticmethod
+    def export_event(obj: BaseEvent) -> str:
+        """
+        Prepare Event data for exporting to text file
+        Returns:
+            Formatted basic string
+        """
+        return f"{obj.id}@{obj.name}" \
+               f"@{obj.date.strftime('%m/%d/%Y')}@{obj.definition}" \
+               f"@{obj.annotation}@{obj.suffix}"
+
+    @staticmethod
+    def export_syllable(obj) -> str:
+        """
+        Prepare Syllable data for exporting to text file
+        Returns:
+            Formatted basic string
+        """
+        return f"{obj.name}@{obj.type}@{obj.allowed}"
+
+    @staticmethod
+    def export_setting(obj) -> str:
+        """
+        Prepare Setting data for exporting to text file
+        Returns:
+            Formatted basic string
+        """
+        return f"{obj.date.strftime('%d.%m.%Y %H:%M:%S')}" \
+               f"@{obj.db_version}" \
+               f"@{obj.last_word_id}" \
+               f"@{obj.db_release}"
+
+    @staticmethod
+    def export_type(obj) -> str:
+        """
+        Prepare Type data for exporting to text file
+        Returns:
+            Formatted basic string
+        """
+        return f"{obj.type}@{obj.type_x}@{obj.group}@{obj.parentable}" \
+               f"@{obj.description if obj.description else ''}"
+
+    @staticmethod
+    def export_word(obj) -> str:
+        """
+        Prepare Word data for exporting to text file
+        Returns:
+            Formatted basic string
+        """
+        ewc = ExportWordConverter(obj)
+        match = ewc.stringer(obj.match)
+        tid_old = ewc.stringer(obj.tid_old)
+        origin_x = ewc.stringer(obj.origin_x)
+        origin = ewc.stringer(obj.origin)
+
+        return f"{obj.id_old}@{obj.type.type}@{obj.type.type_x}@{ewc.e_affixes}" \
+               f"@{match}@{ewc.e_source}@{ewc.e_year}@{ewc.e_rank}" \
+               f"@{origin}@{origin_x}@{ewc.e_usedin}@{tid_old}"
+
+    @staticmethod
+    def export_definition(obj) -> str:
+        """
+        Prepare Definition data for exporting to text file
+        Returns:
+            Formatted basic string
+        """
+        e_grammar = f"{obj.slots if obj.slots else ''}" \
+            f"{obj.grammar_code if obj.grammar_code else ''}"
+
+        return f"{obj.source_word.id_old}@{obj.position}@{obj.usage if obj.usage else ''}" \
+               f"@{e_grammar}@{obj.body}@@{obj.case_tags if obj.case_tags else ''}"
+
+    @staticmethod
+    def export_word_spell(obj) -> str:
+        """
+        Prepare WordSpell data for exporting to text file
+        Returns:
+            Formatted basic string
+        """
+        code_name = "".join("0" if symbol.isupper() else "5" for symbol in str(obj.name))
+
+        return f"{obj.id_old}@{obj.name}@{obj.name.lower()}@{code_name}" \
+               f"@{obj.event_start_id}@{obj.event_end_id if obj.event_end else 9999}@"
+
+
+class ExportWordConverter:
+    """
+    Addon for ExportWord class with converters for properties
+    """
+
+    def __init__(self, word: BaseWord):
+        self.word = word
+
+    @property
+    def e_source(self) -> str:
+        """
+        Returns:
+        """
+        source = '/'.join(sorted([author.abbreviation for author in self.word.authors]))
+        notes: dict[str, str] = self.word.notes if self.word.notes else {}
+
+        return f"{source} {notes.get('author', str())}".strip()
+
+    @property
+    def e_year(self) -> str:
+        """
+        Returns:
+        """
+        notes: dict[str, str] = self.word.notes if self.word.notes else {}
+        if self.word.year:
+            return f"{self.word.year.year}{notes.get('year', str())}".strip()
+        return ""
+
+    @property
+    def e_usedin(self) -> str:
+        """
+        Returns:
+        """
+        return ' | '.join(cpx.name for cpx in self.word.complexes)
+
+    @property
+    def e_affixes(self) -> str:
+        """
+        Returns:
+        """
+        return ' '.join(afx.name.replace("-", "") for afx in self.word.affixes).strip()
+
+    @property
+    def e_rank(self) -> str:
+        """
+        Returns:
+        """
+        notes: dict[str, str] = self.word.notes if self.word.notes else {}
+        return f"{self.word.rank} {notes.get('rank', str())}".strip()
+
+    @staticmethod
+    def stringer(value) -> str:
+        """
+        Convert variable to string
+        Args:
+            value:
+
+        Returns:
+
+        """
+        return str(value) if value else str()
---
+++ tests/loglan_core/test_addons/test_exporter.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=R0201, R0903
+
+"""Export Model unit tests."""
+
+import pytest
+
+from loglan_core.addons.exporter import Exporter
+from loglan_core import Author, Word, Event, Syllable, Setting, Type, Definition
+
+@pytest.mark.usefixtures("db_session")
+class TestExporter:
+
+    e = Exporter()
+
+    def test_wrong_type_export(self):
+        class TestClass:
+            pass
+
+        with pytest.raises(ValueError) as _:
+            self.e.export(TestClass())
+
+    def test_export_word(self, db_session):
+        """Test Word.export() method"""
+        obj = db_session.query(Word).filter(Word.name == "kakto").scalar()
+        result = self.e.export(obj)
+        assert result == (
+            '3880@C-Prim@Predicate@kak kao@56%@L4@1975@1.0'
+            '@3/3R akt | 4/4S acto | 3/3F '
+            'acte | 2/3E act | 2/3H kam@@prukao@'
+        )
+
+    def test_export_author(self, db_session):
+        """Test Author.export() method"""
+        obj = db_session.query(Author).filter(Author.abbreviation == "JCB").scalar()
+        result = self.e.export(obj)
+        assert result == 'JCB@James Cooke Brown@'
+
+    def test_export_event(self, db_session):
+        """Test Event.export() method"""
+        obj = db_session.query(Event).filter(Event.annotation == "Initial").scalar()
+        result = self.e.export(obj)
+        assert result == '1@Start@01/01/1975@The initial vocabulary before updates.@Initial@INIT'
+
+    def test_export_syllable(self, db_session):
+        """Test Syllable.export() method"""
+        obj = db_session.query(Syllable).filter(Syllable.name == "vr").scalar()
+        result = self.e.export(obj)
+        assert result == "vr@InitialCC@True"
+
+    def test_export_setting(self, db_session):
+        """Test Setting.export() method"""
+        obj = db_session.query(Setting).filter(Setting.db_release == "4.5.9").scalar()
+        result = self.e.export(obj)
+        assert result == "09.10.2020 09:10:20@2@10141@4.5.9"
+
+    def test_export_type(self, db_session):
+        """Test Type.export() method"""
+        obj = db_session.query(Type).filter(Type.type == "2-Cpx").scalar()
+        result = self.e.export(obj)
+        assert result == "2-Cpx@Predicate@Cpx@True@Two-term Complex " \
+                         "E.g. flicea, from fli(du)+ce(nj)a=liquid-become."
+
+
+    def test_export_definition(self, db_session):
+        """Test Definition.export() method"""
+        obj = db_session.query(Definition).filter(Definition.body == 'K «test»/«examine» B for P with test V.').scalar()
+        result = self.e.export(obj)
+        assert result == "7191@1@@4v@K «test»/«examine» B for P with test V.@@K-BPV"
+
+
+    def test_export_word_spell(self, db_session):
+        """Test WordSpell.export() method"""
+        obj = db_session.query(Word).filter(Word.name == "prukao").scalar()
+        result = self.e.export_word_spell(obj)
+        assert result == "7191@prukao@prukao@555555@3@9999@"
