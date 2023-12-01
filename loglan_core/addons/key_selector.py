a737d6dda3ba6beecf078eb992f15bacc79eed2a
torrua <torrua@users.noreply.github.com>
2023-12-01 15:48:52 +0700

    Add KeySelector docstrings

--- loglan_core/addons/key_selector.py
+++ loglan_core/addons/key_selector.py
@@ -16,22 +16,24 @@ from loglan_core.word import BaseWord
 
 
 class KeySelector(Select):  # pylint: disable=too-many-ancestors
-    """KeySelector model"""
+    """
+    A class used to select keys meeting certain criteria.
+
+    Attributes:
+        class_ (Type[BaseKey]): The class to be used as the base key. It must be a subclass of BaseKey.
+        is_sqlite (bool): Indicator if the object is being used with SQLite or not.
+    """
 
     def __init__(self, class_=BaseKey, is_sqlite: bool = False) -> None:
         """
-        Initializes the object with the given parameters.
+        Initializes the KeySelector object with the provided parameters.
 
         Args:
-            class_ (Type[BaseKey]): The class to be used as the base key.
-            Must be a subclass of BaseKey.
+            class_ (Type[BaseKey]): The class to be used as the base key. Must be a subclass of BaseKey.
             is_sqlite (bool): Whether the object is being used with SQLite or not.
 
         Raises:
             ValueError: If the provided class_ is not a subclass of BaseKey.
-
-        Returns:
-            None
         """
         if not issubclass(class_, BaseKey):
             raise ValueError(
@@ -49,11 +51,14 @@ class KeySelector(Select):  # pylint: disable=too-many-ancestors
         return True
 
     def by_event(self, event_id: int | None = None) -> KeySelector:
-        """Select query filtered by specified Event (the latest by default)
+        """
+        Filters the select query by the specified Event.
 
         Args:
-          event_id: int
-        Returns: self object with filter applied
+            event_id (int): The identifier of the event to filter by.
+
+        Returns:
+            A KeySelector object with the filter applied.
         """
 
         subquery = (
@@ -67,22 +72,28 @@ class KeySelector(Select):  # pylint: disable=too-many-ancestors
         return self.where(self.class_.id.in_(subquery))
 
     def by_key(self, key: str, case_sensitive: bool = False) -> KeySelector:
-        """Select query filtered by specified searching word
+        """
+        Filters the select query by a specified key.
 
         Args:
-          key: str
-          case_sensitive: bool
-        Returns: self object with filter applied
+            key (str): The key to filter by.
+            case_sensitive (bool): Whether the key search should be case-sensitive.
+
+        Returns:
+            A KeySelector object with the filter applied.
         """
         return self.where(
             self.class_.filter_by_key_cs(key, case_sensitive, self.is_sqlite)
         )
 
     def by_language(self, language: str | None = None) -> KeySelector:
-        """Select query filtered by specified language
+        """
+        Filters the select query by a specified language.
 
         Args:
-          language: str
-        Returns: self object with filter applied
+            language (str): The language to filter by.
+
+        Returns:
+            A KeySelector object with the filter applied.
         """
         return self.where(self.class_.filter_by_language(language))
