7793f23511a6bf7a64c53ff9f66e556458b0519a
torrua <torrua@users.noreply.github.com>
2024-09-07 17:27:42 +0700

    Delete test for Event.latest and Event.latest_id

--- tests/loglan_core/test_event.py
+++ tests/loglan_core/test_event.py
@@ -16,7 +16,3 @@ class TestEvent:
     def test_appeared_words(self, db_session):
         event_5: Event = db_session.query(Event).filter(Event.id == 5).first()
         assert len(event_5.appeared_words) == 3
-
-    def test_latest(self, db_session):
-        latest_event: Event = db_session.execute(Event.latest()).scalar()
-        assert latest_event.id == 6
