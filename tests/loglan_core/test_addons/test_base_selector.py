e6b371756ef9e185ee796fb20151064aa69766f1
torrua <torrua@users.noreply.github.com>
2024-09-08 20:18:55 +0700

    Remove use_wildcard test

--- tests/loglan_core/test_addons/test_base_selector.py
+++ tests/loglan_core/test_addons/test_base_selector.py
@@ -16,8 +16,3 @@ class TestBaseSelector:
     def test_condition_by_attribute_wrong_attr(self, db_session):
         with pytest.raises(AttributeError) as _:
             BaseSelector.condition_by_attribute(Word, "test", "test")
-
-    def test_condition_by_attribute_no_use_wildcard(self, db_session):
-        result = BaseSelector.condition_by_attribute(Word, "name", "kakt*", use_wildcard=False)
-        result_from_db = WordSelector(is_sqlite=True).where(result).all(db_session)
-        assert not len(result_from_db)
