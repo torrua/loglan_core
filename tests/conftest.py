2501b868763750f60da282aac12834da90c62636
torrua <torrua@users.noreply.github.com>
2024-10-29 23:51:09 +0700

    refactor conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,11 @@ from sqlalchemy.orm import scoped_session, sessionmaker, Session
 
 from loglan_core import Syllable, Setting
 from loglan_core.base import BaseModel as Base
-from loglan_core.connect_tables import t_connect_words, t_connect_authors, t_connect_keys
+from loglan_core.connect_tables import (
+    t_connect_words,
+    t_connect_authors,
+    t_connect_keys,
+)
 from tests.data import Author, authors
 from tests.data import Definition, definitions
 from tests.data import Event, events
@@ -15,7 +19,7 @@ from tests.data import connect_words, connect_authors, connect_keys
 from tests.data import syllables, settings
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def db_engine():
     """yields a SQLAlchemy engine which is suppressed after the test session"""
     engine_ = create_engine("sqlite://", echo=False)
@@ -25,13 +29,13 @@ def db_engine():
     engine_.dispose()
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def db_session_factory(db_engine):
     """returns a SQLAlchemy scoped session factory"""
     return scoped_session(sessionmaker(bind=db_engine, future=True))
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def db_session(db_session_factory):
     """yields a SQLAlchemy connection which is rollbacked after the test"""
     session_: Session = db_session_factory()
@@ -44,25 +48,11 @@ def db_session(db_session_factory):
 
 
 def create_db(session):
-    words_objects = [Word(**obj) for obj in words + changed_words]
-    types_objects = [Type(**obj) for obj in types]
-    events_objects = [Event(**obj) for obj in events]
-    authors_objects = [Author(**obj) for obj in authors]
-    definitions_objects = [Definition(**obj) for obj in definitions]
-    keys_objects = [Key(**obj) for obj in keys + un_keys]
-    syllables_objects = [Syllable(**obj) for obj in syllables]
-    settings_objects = [Setting(**obj) for obj in settings]
+    add_objects(session)
+    link_objects(session)
 
-    session.add_all(words_objects)
-    session.add_all(types_objects)
-    session.add_all(events_objects)
-    session.add_all(authors_objects)
-    session.add_all(definitions_objects)
-    session.add_all(keys_objects)
-    session.add_all(syllables_objects)
-    session.add_all(settings_objects)
 
-    session.commit()
+def link_objects(session):
 
     for parent_id, child_id in connect_words:
         ins = t_connect_words.insert().values(parent_id=parent_id, child_id=child_id)
@@ -78,3 +68,31 @@ def create_db(session):
         ins = t_connect_keys.insert().values(KID=key_id, DID=definition_id)
         session.execute(ins)
         session.commit()
+
+
+def add_objects(session):
+    objects = get_objects()
+    for obj in objects:
+        session.add_all(obj)
+    session.commit()
+
+
+def get_objects():
+    words_objects = [Word(**obj) for obj in words + changed_words]
+    types_objects = [Type(**obj) for obj in types]
+    events_objects = [Event(**obj) for obj in events]
+    authors_objects = [Author(**obj) for obj in authors]
+    definitions_objects = [Definition(**obj) for obj in definitions]
+    keys_objects = [Key(**obj) for obj in keys + un_keys]
+    syllables_objects = [Syllable(**obj) for obj in syllables]
+    settings_objects = [Setting(**obj) for obj in settings]
+    return (
+        authors_objects,
+        definitions_objects,
+        events_objects,
+        keys_objects,
+        settings_objects,
+        syllables_objects,
+        types_objects,
+        words_objects,
+    )
