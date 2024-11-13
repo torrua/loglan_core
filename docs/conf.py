4b4b03a7ec4e16ab6c4217ac1c64dbe0290bf66b
torrua <torrua@users.noreply.github.com>
2024-11-13 19:46:08 +0300

    Add rst files

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,8 @@
 # Configuration file for Sphinx
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../"))
 
 # -- Project information -----------------------------------------------------
 project = "Loglan-Core"
@@ -11,15 +15,28 @@ extensions = [
     "sphinx_rtd_theme",
     "sphinx.ext.autodoc",
     "sphinx.ext.viewcode",  # Optional: to include source code in the documentation
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",  # For type hints in docstrings
 ]
+
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = True
+napoleon_include_private_with_doc = True
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = True
+napoleon_use_admonition_for_notes = True
+napoleon_use_admonition_for_references = True
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_preprocess_types = False
+napoleon_type_aliases = None
+napoleon_attr_annotations = True
+
 html_theme = "sphinx_rtd_theme"
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
-# -- Autodoc configuration ---------------------------------------------------
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath("../"))
 
 # sphinx-build -b html . _build/html
---
+++ docs/loglan_core.service.rst
@@ -0,0 +1,8 @@
+Service Modules
+===========================
+
+.. toctree::
+   :maxdepth: 1
+
+   service/annotated_types
+   service/table_names
